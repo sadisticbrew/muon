3751a3f9bb87fc12b9e7ecd80055214f8c3221e0
Pratham <prathaminluck@gmail.com>
2026-04-24 16:01:37 +0530

    feat: Add connect, exit syscall tracing
    This commit adds tracing for the `connect`, `exit`syscalls.
    The `connect` syscall tracing includes the raw address information for
    network connections.
    The `exit` syscall tracing logs the PID and command of the exiting
    process.
    The `openat` syscall tracing has been temporarily ignored due to its
    high volume.

--- bpf/muon.c
+++ bpf/muon.c
@@ -2,32 +2,37 @@
 
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
-// License must be GPL-compatible to use many eBPF helpers
+
 char __license[] SEC("license") = "Dual MIT/GPL";
 
-// The data we want to send to user space
 struct event {
     __u32 pid;
     __u32 type;
     char comm[16];
     char fname[256];
-
+    // struct connect_call call;
+    unsigned char raw_addr[128];
 };
 
-// Define a ring buffer map
+
+
 struct {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
     __uint(max_entries, 1 << 24); // 16 MB buffer
+    // __uint(max_entries, 256 * 1024); // 16 MB buffer
+
 } events SEC(".maps");
 
-// Attach to the openat syscall tracepoint
 SEC("tracepoint/syscalls/sys_enter_execve")
 int trace_execve(struct trace_event_raw_sys_enter *ctx) {
     struct event *e;
 
-    // Reserve space in the ring buffer
     e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
-    if (!e) return 0; // Drop event if buffer is full
+    if (!e) {
+        static const char fmt[] = "THE BUFFER IS FULL!";
+        bpf_trace_printk(fmt, sizeof(fmt));
+        return 0;} // Drop event if buffer is full
+    e->type = 0;
     // bpf_get_current_pid_tgid() returns the thread group ID (which user space calls PID)
     // in the upper 32 bits, and the thread ID in the lower 32 bits.
     e->pid = bpf_get_current_pid_tgid() >> 32;
@@ -37,10 +42,8 @@ int trace_execve(struct trace_event_raw_sys_enter *ctx) {
     int res = bpf_probe_read_user_str(&e->fname, sizeof(e->fname), filename);
 
 
-    e->type = 0;
-    // Submit the event
-    bpf_ringbuf_submit(e, 0);
 
+    bpf_ringbuf_submit(e, 0);
 
     return 0;
 }
@@ -51,6 +54,7 @@ int trace_openat(struct trace_event_raw_sys_enter *ctx) {
 
     e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
     if (!e) return 0;
+    e->type = 1;
 
     e->pid = bpf_get_current_pid_tgid() >> 32;
 
@@ -59,7 +63,52 @@ int trace_openat(struct trace_event_raw_sys_enter *ctx) {
     char *fname = (char *)ctx->args[1];
     int res = bpf_probe_read_user_str(&e->fname, sizeof(e->fname), fname);
 
-    e->type = 1;
+
+    bpf_ringbuf_submit(e, 0);
+
+    return 0;
+}
+
+SEC("tracepoint/syscalls/sys_enter_exit")
+int trace_exit(struct trace_event_raw_sys_enter *ctx) {
+    struct event *e;
+
+    e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
+    if (!e) return 0;
+    e->type = 2;
+
+    e->pid = bpf_get_current_pid_tgid() >> 32;
+
+    bpf_get_current_comm(&e->comm, sizeof(e->comm));
+
+    int res = (int)ctx->args[0];
+    if (res != 0) {
+
+        bpf_ringbuf_discard(e, 0);
+        return 0;
+    }
+
+
+    bpf_ringbuf_submit(e, 0);
+
+    return 0;
+}
+
+SEC("tracepoint/syscalls/sys_enter_connect")
+int trace_connect(struct trace_event_raw_sys_enter *ctx) {
+    struct event *e;
+
+    e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
+    if (!e) return 0;
+    e->type = 3;
+
+    e->pid = bpf_get_current_pid_tgid() >> 32;
+
+    bpf_get_current_comm(&e->comm, sizeof(e->comm));
+
+    void *addr_ptr = (void *)ctx->args[1];
+
+    bpf_probe_read_user(&e->raw_addr, sizeof(e->raw_addr), addr_ptr);
 
     bpf_ringbuf_submit(e, 0);
 
